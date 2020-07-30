7b4bc404db7e998cc0ef6685d134dd9ba2a810a7
LordDarkenon <r.ager@hotmail.co.uk>
2020-07-30 14:24:52 +0100

    Final

---
+++ Assets/ScaleCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class ScaleCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        float orthographicsize = (30.22f / (float)Screen.width) * (float)Screen.height / 2.0f;
+
+        if (orthographicsize < 8.5f)
+        {
+            orthographicsize = 8.5f;
+        }
+
+        Camera.main.orthographicSize = orthographicsize;
+    }
+
+#if UNITY_EDITOR
+
+    private void Update()
+    {
+        if (!Application.IsPlaying(gameObject))
+        {
+            float orthographicsize = (30.22f / (float)Screen.width) * (float)Screen.height / 2.0f;
+
+            if (orthographicsize < 8.5f)
+            {
+                orthographicsize = 8.5f;
+            }
+
+            Camera.main.orthographicSize = orthographicsize;
+        }
+    }
+
+#endif
+}
